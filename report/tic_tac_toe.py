10f5456a89370197a3ea6bfe854761270a9ba845
Naruaki TOMA <tnal@ie.u-ryukyu.ac.jp>
2018-05-30 14:31:59 +0900

    modify desc & return value on point_random()

--- report/tic_tac_toe.py
+++ report/tic_tac_toe.py
@@ -28,12 +28,12 @@ Example:
         2. play one hand with `c' randomly.
 
     case 1:
-        % python3
+        % python
         > import tic_tac_toe
         > tic_tac_toe.test_play()
 
     case 2:
-        % python3
+        % python
         > from tic_tac_toe import *
         > test_play()
 """
@@ -43,13 +43,13 @@ import random
 
 def init_board():
     """Return the empty board.
-    
+
     Args:
         None
-    
+
     Returns:
         list: the empty board with 9 items.
-    
+
     >>> board = init_board()
     >>> print(board)
     ['e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e']
@@ -59,19 +59,19 @@ def init_board():
     while (index <= 9):
         board.append('e')
         index += 1
-    
+
     return board
 
 
 def print_board(board):
     """Print the board with 3x3.
-    
+
     Args:
         board (list): the list with 9 items.
-    
+
     Returns:
         None (only print the board to sys.stdout (standard output)
-    
+
     >>> board = init_board()
     >>> print_board(board)
     eee
@@ -86,19 +86,19 @@ def print_board(board):
             print('{0}'.format(state))
         else:
             print('{0}'.format(state), end='')
-    
+
     print('---')
 
 
 def gather_empty_cells(board):
     """Gather empty cells on the board.
-    
+
     Args:
         board (list): the list with 9 items.
-    
+
     Returns:
         list: the index list of empty state on the board.
-    
+
     >>> board = ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'e']
     >>> empty_list = gather_empty_cells(board)
     >>> len(empty_list) == 1
@@ -112,21 +112,21 @@ def gather_empty_cells(board):
         if state == 'e':
             empty_list.append(index)
         index += 1
-    
+
     return empty_list
 
 
 def point_random(board, player):
     """Play one step randomly.
-    
+
     Args:
         board (list): the list with 9 items.
         player (str): 'o' for circle player, 'x' for cross player
-    
+
     Returns:
         #####board (list): the point one step randomly with the player.
         point (int): the pointed index on the board
-    
+
     Note:
         This function DOESN'T check the validity of turn.
         You can play oneside-game.
@@ -139,14 +139,14 @@ def point_random(board, player):
     empty_list = gather_empty_cells(board)
     point = random.randint(0, len(empty_list)-1)
     board[empty_list[point]] = player
-    return point
+    return empty_list[point]
 
 
 def test_play():
     board = init_board()
     print('# board was initialized.')
     print_board(board)
-    
+
     index = point_random(board, 'o')
     print("# user 'o' pointed to board[{0}]".format(index))
     print_board(board)
@@ -154,10 +154,3 @@ def test_play():
     index = point_random(board, 'x')
     print("# user 'x' pointed to board[{0}]".format(index))
     print_board(board)
-
-
-if __name__ == '__main__':
-    import doctest
-    doctest.testmod()
-
-
